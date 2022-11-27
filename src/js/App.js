3a917f569279ad8d6bc9bc86f147c8e0f9c9f141
vagabondachi <pearlybruno11@gmail.com>
2022-11-27 19:10:25 +0800

    just doc reformat

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,15 @@
-import React, {useEffect} from 'react';
-import { useDispatch, useSelector} from 'react-redux'; 
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import StoreProvider from './store/StoreProvider';
 
-import { 
-  HashRouter as Router, 
-  Route, 
+import {
+  HashRouter as Router,
+  Route,
   Switch,
   Redirect
 } from "react-router-dom";
 
-import { listenToAuthChanges } from './actions/auth'; 
+import { listenToAuthChanges } from './actions/auth';
 import { listenToConnectionChanges } from './actions/app';
 import { checkUserConnection } from './actions/connection';
 
@@ -18,24 +18,23 @@ import ChatCreate from './views/ChatCreate';
 import WelcomeView from './views/Welcome';
 import SettingsView from './views/Settings';
 import LoadingView from './components/shared/LoadingView';
-import GeneralApp from './dashboard/GeneralApp';
 import DashboardLayout from './layouts/dashboard';
 
 
 
-function AuthRoute({children, ...rest}) {
-  const user = useSelector(({auth})=> auth.user)
+function AuthRoute({ children, ...rest }) {
+  const user = useSelector(({ auth }) => auth.user)
   const onlyChild = React.Children.only(children);
- 
+
 
   return (
     <Route
-    {...rest}
-    render = {props =>
-      user ? 
-        React.cloneElement(onlyChild, {...rest, ...props}) :
-        <Redirect to="/" />
-    }
+      {...rest}
+      render={props =>
+        user ?
+          React.cloneElement(onlyChild, { ...rest, ...props }) :
+          <Redirect to="/" />
+      }
     />
   )
 }
@@ -44,14 +43,12 @@ function AuthRoute({children, ...rest}) {
 
 function ChatApp() {
   const dispatch = useDispatch();
-  const isChecking = useSelector(({auth}) => auth.isChecking);
-  const isOnline = useSelector(({app}) => app.isOnline);
+  const isChecking = useSelector(({ auth }) => auth.isChecking);
+  const isOnline = useSelector(({ app }) => app.isOnline);
 
-  // Wrapper
-  const ContentWrapper = ({children}) => <div>{children}</div>
-  
 
-  useEffect(()=>{
+
+  useEffect(() => {
     const unsubFromAuth = dispatch(listenToAuthChanges());
     const unsubFromConnection = dispatch(listenToConnectionChanges());
     const unsubFromUserConnection = dispatch(checkUserConnection());
@@ -61,44 +58,42 @@ function ChatApp() {
       unsubFromAuth();
       unsubFromConnection();
       unsubFromUserConnection();
-  
+
     }
   }, [dispatch])
 
-  if(!isOnline){
-    return<LoadingView message="Application has been disconnected from the internet. Please reconnect..."/>
+  if (!isOnline) {
+    return <LoadingView message="Application has been disconnected from the internet. Please reconnect..." />
   }
 
-  if(isChecking){
-    return<LoadingView/>
+  if (isChecking) {
+    return <LoadingView />
   }
- 
- 
+
+
   return (
     <Router>
-    
-        <Switch>
-          <Route path="/" exact>
-            <WelcomeView />
-          </Route>
 
-          {/* Need to wrap the GeneralApp with the Homeview */}
-    
-          <AuthRoute path="/home">
-            <DashboardLayout/>
-           
-            </AuthRoute>
-          
-        </Switch>
-       
-    </Router>  
+      <Switch>
+        <Route path="/" exact>
+          <WelcomeView />
+        </Route>
+
+
+        <AuthRoute path="/home">
+          <DashboardLayout />
+        </AuthRoute>
+
+      </Switch>
+
+    </Router>
   )
 }
 
 export default function App() {
   return (
     <StoreProvider>
-      <ChatApp/>
+      <ChatApp />
     </StoreProvider>
   )
 }
--- src/js/layouts/dashboard/SideNav.js
+++ src/js/layouts/dashboard/SideNav.js
@@ -26,20 +26,20 @@
 // import Dashboard from '../components/Dashboard';
 
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
-import { useTheme,styled } from '@mui/material/styles';
+import { useTheme, styled } from '@mui/material/styles';
 import { Nav_Buttons } from '../../data';
 import { useSelector } from 'react-redux';
 
-import { 
+import {
   RiSettings2Fill
- } from "react-icons/ri";
+} from "react-icons/ri";
 import GeneralApp from '../../dashboard/GeneralApp';
 import useSettings from '../../hooks/useSettings'
 
 
- const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
   width: 52,
   height: 35,
   padding: 7,
@@ -89,206 +89,139 @@ import useSettings from '../../hooks/useSettings'
 
 
 
- const DashboardLayout = () => {
-//   const[isOpen ,setIsOpen] = useState(false);
-//   const toggle = () => setIsOpen (!isOpen);
-//   const menuItem=[
-//     {
-//       path: "/",
-//       name: "Dashboard",
-//       icon: <RiNavigationFill/>
-//     },
-//     {
-//       path: "/home/direct",
-//       name: "Direct Message",
-//       icon: <RiNavigationFill/>
-//     },
-//     {
-//       path: "/home/team",
-//       name: "Team Message",
-//       icon: <RiGamepadFill/>
-//     },
-//   ]
-//   return (
+const DashboardLayout = () => {
 
-//     <div className='page'>
-//       <div style={{width: isOpen ? "15%" : "5%"}}className='sidemenu'>
-//         <div className='top_section'>
-//           <h1 style={{display: isOpen ? "block" : "none"}} className='logo'> Weedle </h1>
-//             <div style={{marginLeft: isOpen ? "5%" : "0%"}} className='bars'> 
-//             <img src='C:/Users/pearl/OneDrive/Desktop/Weedle/assets/images/smallweedle.png'onClick={toggle}/> 
-//             </div>
-//         </div> 
-//       {
-//         menuItem.map((item, index)=>(
-//           <NavLink to={item.path} key={index} className="link" activeClassName="active">
-//             <div className='icon'>{item.icon}</div>
-//             <div style={{display: isOpen ? "block" : "none"}} className='link_text'>{item.name}</div>
-//           </NavLink>
-//         ))
-//       }
-//        </div>
-     
-//       <Contents/>
-  
+  const user = useSelector(({ auth }) => auth.user)
+  const theme = useTheme();
+  const [selected, setSelected] = useState(0);
+  console.log(theme);
+  const { onToggleMode } = useSettings();
 
-//       </div>
-  
-//   )
-// }
+  return (
+    <>
+      <Stack direction="row">
+        <Box p={2}
+          sx={{
+            backgroundColor: theme.palette.background.paper,
+            boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+            height: "100vh",
+            width: 100
 
-// function Contents(){
-//   const dispatch = useDispatch();
-//   const joinedChats = useSelector(({chats}) => chats.joined)
+          }}>
 
 
-//   useEffect(() => {
-//     Notification.setup();
-//     dispatch(fetchChats())
-//   }, [dispatch])
+          {/* Stack for logo,icon,avatar */}
+          <Stack
+            sx={{
+              height: '90vh'
+            }}
+            direction="column"
+            alignItems="center"
+            justifyContent="space-between"
+            spacing={3}
 
-//   return(
- 
-//       <Router>
-//         <Route path="/">
-//             <Dashboard/>
-//           </Route >
-//           <Route  path="/home/direct">
-//             <DirectMsg/>
-//           </Route >
-//           <Route  path="/home/team">
-//             <TeamMsg chats={joinedChats} />
-//             </Route >
-//         </Router>
-   
-    
-//   )
-const user = useSelector(({auth}) => auth.user)
-const theme = useTheme();
-const [selected,setSelected] = useState(0);
-console.log(theme);
-const {onToggleMode} = useSettings();
-
-return(
-  <>
-  <Stack direction = "row">
-  <Box p={2}
-    sx ={{
-      backgroundColor: theme.palette.background.paper,
-      boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
-      height: "100vh",
-      width:100
-    
-    }}>
-
-
-      {/* Stack for logo,icon,avatar */}
-      <Stack 
-        sx={{
-        height: '90vh'}}
-        direction="column"
-        alignItems="center" 
-        justifyContent="space-between"
-        spacing= {3}
-
-      >
-        {/* Stack for logo and icons only */}
-
-        <Stack alignItems={"center"} spacing={4}>
-
-        {/* background container for Logo */}
-        <Box p={2} sx={{
-          backgroundColor: theme.palette.primary.main,
-          height: 64,
-          width: 64,
-          borderRadius: 1.5,
-          }}
           >
-         {/* Logo */}
-            <img src='C:/Users/pearl/OneDrive/Desktop/Weedle/assets/images/smallweedle.png' alt="chat logo"/>
-            </Box>
+            {/* Stack for logo and icons only */}
 
-          {/* Stack for icons   */}
-            <Stack 
-              sx={{width: "max-content"}}   
-              direction="column"
-              alignItems="center"         
-              spacing= {3}
-            >
-              {Nav_Buttons.map((el) => 
+            <Stack alignItems={"center"} spacing={4}>
+
+              {/* background container for Logo */}
+              <Box p={2} sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
+              }}
+              >
+                {/* Logo */}
+                <img src='C:/Users/pearl/OneDrive/Desktop/Weedle/assets/images/smallweedle.png' alt="chat logo" />
+              </Box>
+
+              {/* Stack for icons   */}
+              <Stack
+                sx={{ width: "max-content" }}
+                direction="column"
+                alignItems="center"
+                spacing={3}
+              >
+                {Nav_Buttons.map((el) =>
                   el.index === selected ? (
-                <Box 
-                  p={1}
-                  sx={{
-                  backgroundColor: theme.palette.primary.main, 
-                  borderRadius: 1.5
-                   }}
+                    <Box
+                      p={1}
+                      sx={{
+                        backgroundColor: theme.palette.primary.main,
+                        borderRadius: 1.5
+                      }}
+                    >
+                      <IconButton
+                        sx={{ width: "max-content", color: "#fff" }}
+                        key={el.index}
+                      >
+                        {el.icon}
+                      </IconButton>
+                    </Box>
+                  ) : (
+                    <IconButton
+                      onClick={() => {
+                        setSelected(el.index);
+                      }}
+                      sx=
+                      {{
+                        width: "max-content",
+                        color: "#000"
+                      }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  )
+                )}
+
+                <Divider sx={{ width: "48px" }} />
+                {selected === 3 ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5
+                    }}
                   >
-                  <IconButton 
-                    sx={{width: "max-content", color: "#fff"}}
-                    key={el.index}
-                  > 
-                    {el.icon} 
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}>
+                      <RiSettings2Fill />
                     </IconButton>
                   </Box>
-                ) : (    
-                  <IconButton 
-                  onClick={() => {
-                    setSelected(el.index);
-                  }}
-                  sx=
-                  {{width: "max-content",
-                  color: "#000"
-                  }}
-                  key={el.index}
-                  > 
-                  {el.icon} 
-                  </IconButton>
-                  )
-                  )}
-                  
-                  <Divider sx={{ width: "48px" }} />
-                  {selected === 3 ? ( 
-                    <Box 
-                    p={1}
-                    sx={{backgroundColor: theme.palette.primary.main, 
-                    borderRadius: 1.5
-                  }}
-                    >
-                        <IconButton
-                          sx={{width: "max-content", color: "#fff"}}>
-                    <RiSettings2Fill/>
-                  </IconButton>   
-                  </Box>
-                   ) : ( 
-                   
-                   <IconButton onClick={() => {
+                ) : (
+
+                  <IconButton onClick={() => {
                     setSelected(3);
                   }}
-                  sx={{width: "max-content",
-                  color: "#000"}}
+                    sx={{
+                      width: "max-content",
+                      color: "#000"
+                    }}
                   >
-                  <RiSettings2Fill/>
-                </IconButton>   
-                )}   
+                    <RiSettings2Fill />
+                  </IconButton>
+                )}
 
-        </Stack>
+              </Stack>
             </Stack>
 
-            
-         <Stack spacing={4}>
-          <MaterialUISwitch onChange={()=> {
-            onToggleMode();
 
-          }}
-              defaultChecked />
-            <Avatar src={user.avatar}/>
+            <Stack spacing={4}>
+              <MaterialUISwitch onChange={() => {
+                onToggleMode();
+
+              }}
+                defaultChecked />
+              <Avatar src={user.avatar} />
             </Stack>
-       </Stack>
-    </Box>
-    <GeneralApp/>
-    </Stack>
+          </Stack>
+        </Box>
+        <GeneralApp />
+      </Stack>
     </>
-)
+  )
 }
 export default DashboardLayout;
